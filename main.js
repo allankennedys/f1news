d6357abf5c67fe9254c7ca9eeb44fb0b0969a7d4
allankennedys <kennedyyemail@gmail.com>
2024-12-30 00:11:58 -0300

    update

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ function getNews(){
     fetch('https://newsapi.org/v2/everything?q=f1&language=pt&apiKey=81744279e9d247f68fa29c098eb868f3')
     .then(response => {
         if(!response.ok){
-            throw Error("ERROR");
-        }
+            throw Error("ERROR");        }
         else{
             return response.json();
         }
@@ -46,6 +45,7 @@ function getNews(){
 }})
     .catch(error => {
         console.log(error);
+        newsArea.innerHTML = 'Devido às limitações do plano gratuito da API utilizada para fornecer as notícias, o acesso às notícias em produção não é permitido diretamente via GitHub Pages. Para visualizar as notícias mais recentes, é necessário rodar o projeto localmente em sua máquina.';
     })
 }
 
