87fc5b442275e1741e4406596d74b188c2ba6935
allankennedys <kennedyyemail@gmail.com>
2025-06-29 01:14:04 -0300

    newapi

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+import API_KEY from './config.js';
+  
+  function formatarDataBR(dataISO) {
+  const data = new Date(dataISO);
+  const dia = String(data.getDate()).padStart(2, '0');
+  const mes = String(data.getMonth() + 1).padStart(2, '0');
+  const ano = data.getFullYear();
+  const horas = String(data.getHours()).padStart(2, '0');
+  const minutos = String(data.getMinutes()).padStart(2, '0');
+
+  return `${dia}/${mes}/${ano} ${horas}:${minutos}`;
+}
+
+
+const dataOriginal = "2023-10-05T14:30:00Z";
+const dataFormatada = formatarDataBR(dataOriginal);
+console.log(dataFormatada);
+
 
 function getNews(){
-    fetch('https://newsapi.org/v2/everything?q=f1&language=pt&apiKey=[API_KEY]')
+
+    fetch(`https://gnews.io/api/v4/search?q=formula 1&lang=pt&apikey=${API_KEY}`)
     .then(response => {
         if(!response.ok){
             throw Error("ERROR");        }
@@ -14,33 +33,49 @@ function getNews(){
     const newsArea = document.getElementById('newsArea');
     const articles = data.articles;
     
+    
     for(i=0; i<articles.length; i++){
 
         const newsContainer = document.createElement('div');
 
-        const titulo = document.createElement('h1');
+        const titulo = document.createElement('h2');
         const autor = document.createElement('p');
         const descricao = document.createElement('h5');
         const data = document.createElement('p');
         const link = document.createElement('a');
+        const image = document.createElement('img');
+        const btn = document.createElement('a');
         
         newsArea.appendChild(newsContainer);
+        image.classList.add('newsImage');
+        
+        const dataPublicacao = formatarDataBR(articles[i].publishedAt);
+
+
 
 
         titulo.innerHTML = articles[i].title;
-        autor.innerHTML = `Por ${articles[i].author}`;
-        data.innerHTML = `Publicado em ${articles[i].publishedAt}`;
+        autor.innerHTML = `Por ${articles[i].source.name}`;
+        data.innerHTML = `Publicado em ${dataPublicacao}`;
         descricao.innerHTML = articles[i].description;
-        link.innerHTML = 'Leia mais';
+        btn.innerHTML = 'Leia mais';
         link.href = articles[i].url;
         link.target = '_blank';
-
-        newsContainer.appendChild(titulo);
+        image.src = articles[i].image;
+        btn.setAttribute('href', articles[i].url);
+        btn.classList.add('btn');
+        link.classList.add('newsTitle');
+        
+        newsContainer.appendChild(link);
+        link.appendChild(titulo);
+        newsContainer.appendChild(image);
         newsContainer.appendChild(autor);
         newsContainer.appendChild(data);
         newsContainer.appendChild(descricao);
-        newsContainer.appendChild(link);
+        newsContainer.appendChild(btn);
 
+        newsContainer.classList.add('newsContainer');
+        
 
 }})
     .catch(error => {
